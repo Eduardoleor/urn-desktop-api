ed8afc335e4010fea2012f3042afea8c7750088a
eduardo.leal <eduardo.leal.or@gmail.com>
2023-02-23 01:22:09 -0600

    feature: added cors support and added yarn dependencies of npm

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import express from "express";
+import express from 'express'
+import cors from 'cors'
 
-import diaryRouter from "./routes/diaries";
+import diaryRouter from './routes/diaries'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-const PORT = 3009;
+const PORT = 3009
 
-app.get("/ping", (_, res) => {
-  console.log("ping");
-  res.send("pong");
-});
+app.get('/ping', (_, res) => {
+  console.log('ping')
+  res.send('pong')
+})
 
-app.use("/api/diaries", diaryRouter);
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
